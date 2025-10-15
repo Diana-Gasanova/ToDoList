4967bdc3fb7c8b1b27282ec0ce23b92993c9b05c
Diana <diadia11511@mail.ru>
2025-10-15 11:12:58 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,21 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Diana G on 15.10.2025.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct ToDoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
